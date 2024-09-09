cb24bce3c6949b70ddf22539584c7a50ce6a7f67
TarekHawari <140298262+TarekHawari@users.noreply.github.com>
2024-09-09 21:24:43 +1000

    Added registration UI and changed HelloApplication

--- src/main/java/com/example/cab302project/HelloApplication.java
+++ src/main/java/com/example/cab302project/HelloApplication.java
@@ -11,10 +11,11 @@ public class HelloApplication extends Application {
     public static final String TITLE = "Student Hub";
     public static final int WIDTH = 770;
     public static final int HEIGHT = 560;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("nav-bar.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
+        FXMLLoader fxmlRegistration = new FXMLLoader(HelloApplication.class.getResource("register-ui.fxml"));
+        Scene scene = new Scene(fxmlRegistration.load(), WIDTH, HEIGHT);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -23,4 +24,5 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }
