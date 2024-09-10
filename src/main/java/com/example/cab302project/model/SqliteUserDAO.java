a301f2d61894ff52326875c93bc421f9d6275e3a
LucasKimo <kesgoodman7@gmail.com>
2024-09-10 13:04:30 +1000

    Database

---
+++ src/main/java/com/example/cab302project/model/SqliteUserDAO.java
@@ -0,0 +1,151 @@
+package com.example.cab302project.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SqliteUserDAO implements IUserDAO {
+    private Connection connection;
+
+    public SqliteUserDAO() {
+        connection = SqliteConnection.getInstance();
+        createTable();
+    }
+
+    private void createTable() {
+
+        try {
+            Statement statement = connection.createStatement();
+            String query = "CREATE TABLE IF NOT EXISTS users ("
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "username VARCHAR NOT NULL,"
+                    + "password VARCHAR NOT NULL,"
+                    + "firstName VARCHAR NOT NULL,"
+                    + "lastName VARCHAR NOT NULL,"
+//                    + "school VARCHAR NULL,"
+//                    + "email VARCHAR NULL,"
+//                    + "phone VARCHAR NULL,"
+//                    + "address VARCHAR NULL"
+                    + ")";
+            statement.execute(query);
+        } catch( Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void addUser(User user) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO users (firstName, lastName, username, password) VALUES (?, ?, ?, ?)");
+            statement.setString(1, user.getFirstName());
+            statement.setString(2, user.getLastName());
+            statement.setString(3, user.getUsername());
+            statement.setString(4, user.getPassword());
+            statement.executeUpdate();
+            // Set the id of the new contact
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                user.setId(generatedKeys.getInt(1));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void updateUser(User user) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("UPDATE users SET firstName = ?, lastName = ?, school = ?, email = ?, phone = ?, address = ? WHERE id = ?");
+            statement.setString(1, user.getFirstName());
+            statement.setString(2, user.getLastName());
+            statement.setString(3, user.getUsername());
+            statement.setString(4, user.getPassword());
+//            statement.setString(6, user.getSchool());
+//            statement.setString(7, user.getEmail());
+//            statement.setString(8, user.getPhone());
+//            statement.setString(9, user.getAddress());
+            statement.setInt(5, user.getId());
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void deleteUser(User user) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM users WHERE id = ?");
+            statement.setInt(1, user.getId());
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int searchUser(String username, String password) {
+        return 0;
+    }
+
+    @Override
+    public boolean checkPassword(String userPassword, String password) {
+        return false;
+    }
+
+    @Override
+    public boolean checkUsername(String userUsername, String username) {
+        return false;
+    }
+
+    @Override
+    public User getUser(int id) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM users WHERE id = ?");
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String firstName = resultSet.getString("firstName");
+                String lastName = resultSet.getString("lastName");
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+//                String phone = resultSet.getString("phone");
+//                String email = resultSet.getString("email");
+//                String school = resultSet.getString("school");
+//                String address = resultSet.getString("address");
+                User user = new User(firstName, lastName, username, password);
+                user.setId(id);
+                return user;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String query = "SELECT * FROM users";
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String firstName = resultSet.getString("firstName");
+                String lastName = resultSet.getString("lastName");
+                String phone = resultSet.getString("phone");
+                String email = resultSet.getString("email");
+                User user = new User(firstName, lastName, phone, email);
+                user.setId(id);
+                users.add(user);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+}
+
