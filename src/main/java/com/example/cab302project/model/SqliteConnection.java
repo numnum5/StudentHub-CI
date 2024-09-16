fc66f92a36ed32e434ec8979047b09fa025d7510
numnum5 <127462339+numnum5@users.noreply.github.com>
2024-09-16 12:10:43 +1000

    Fixed

--- src/main/java/com/example/cab302project/model/SqliteConnection.java
+++ src/main/java/com/example/cab302project/model/SqliteConnection.java
@@ -4,17 +4,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SqliteConnection {
+
     private static Connection instance = null;
 
     private SqliteConnection() {
         String url = "jdbc:sqlite:database.db";
         try {
-            
             instance = DriverManager.getConnection(url);
         } catch (SQLException sqlEx) {
             System.err.println(sqlEx);
         }
     }
+
 
     public static Connection getInstance() {
         if (instance == null) {
