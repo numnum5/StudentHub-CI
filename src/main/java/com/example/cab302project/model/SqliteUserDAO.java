80cd70e76c96d3e5ec4ef7cda996df2874d00e81
numnum5 <127462339+numnum5@users.noreply.github.com>
2024-10-25 20:58:13 +1000

    Update SqliteUserDAO.java

--- src/main/java/com/example/cab302project/model/SqliteUserDAO.java
+++ src/main/java/com/example/cab302project/model/SqliteUserDAO.java
@@ -6,17 +6,43 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Implementation of the IUserDAO interface, managing database operations for user data using SQLite.
+ * Provides methods to add, update, delete, and retrieve users, as well as check if user exists and validate passwords.
+ */
 public class SqliteUserDAO implements IUserDAO {
+
+    /**
+     * The database connection used to interact with the SQLite database.
+     */
     private Connection connection;
+
+    /**
+     * Indicates whether the added user is unique (i.e., username is not already in the database).
+     */
     public boolean userUnique;
+
+    /**
+     * Stores the username of the current user (optional, currently unused in class logic).
+     */
     public String userName;
 
+    /**
+     * Constructor initializes the SQLite connection and ensures the "users" table exists.
+     */
     public SqliteUserDAO() {
         this.connection = SqliteConnection.getInstance();
         createTable();
     }
 
+    /**
+     * Creates the "users" table in the SQLite database if it does not already exist.
+     * The table contains fields for id, username, password, first name, and last name.
+     */
     private void createTable() {
         try {
             Statement statement = connection.createStatement();
@@ -26,10 +52,6 @@ public class SqliteUserDAO implements IUserDAO {
                     + "password VARCHAR NOT NULL,"
                     + "firstName VARCHAR NOT NULL,"
                     + "lastName VARCHAR NOT NULL"
-//                    + "school VARCHAR NULL,"
-//                    + "email VARCHAR NULL,"
-//                    + "phone VARCHAR NULL,"
-//                    + "address VARCHAR NULL"
                     + ")";
             statement.execute(query);
         } catch (Exception e) {
@@ -37,44 +59,53 @@ public class SqliteUserDAO implements IUserDAO {
         }
     }
 
+    /**
+     * Adds a new user to the "users" table in the database.
+     * If the user is successfully added, the user's id is set.
+     *
+     * @param user The user to be added.
+     */
     @Override
     public void addUser(User user) {
         try {
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO users (firstName, lastName, username, password) VALUES (?, ?, ?, ?)");
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO users (firstName, lastName, username, password) VALUES (?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
             statement.setString(1, user.getFirstName());
             statement.setString(2, user.getLastName());
             statement.setString(3, user.getUsername().toLowerCase());
             statement.setString(4, user.getPassword());
             statement.executeUpdate();
-            // Set the id of the new contact
+
+            // Retrieve the generated ID and set it to the user
             ResultSet generatedKeys = statement.getGeneratedKeys();
             userUnique = true;
             if (generatedKeys.next()) {
                 user.setId(generatedKeys.getInt(1));
             }
-        }
-
-        catch (org.sqlite.SQLiteException e)
-        {
-            userUnique = false;
-        }
-        catch (Exception e) {
+        } catch (org.sqlite.SQLiteException e) {
+            userUnique = false; // Set to false if a unique constraint violation occurs
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Updates the details of an existing user in the "users" table.
+     *
+     * @param user The user with updated information.
+     */
     @Override
     public void updateUser(User user) {
         try {
-            PreparedStatement statement = connection.prepareStatement("UPDATE users SET firstName = ?, lastName = ?, school = ?, email = ?, phone = ?, address = ? WHERE id = ?");
+            PreparedStatement statement = connection.prepareStatement(
+                    "UPDATE users SET firstName = ?, lastName = ?, username = ?, password = ? WHERE id = ?"
+            );
             statement.setString(1, user.getFirstName());
             statement.setString(2, user.getLastName());
             statement.setString(3, user.getUsername());
             statement.setString(4, user.getPassword());
-//            statement.setString(6, user.getSchool());
-//            statement.setString(7, user.getEmail());
-//            statement.setString(8, user.getPhone());
-//            statement.setString(9, user.getAddress());
             statement.setInt(5, user.getId());
             statement.executeUpdate();
         } catch (Exception e) {
@@ -82,10 +113,17 @@ public class SqliteUserDAO implements IUserDAO {
         }
     }
 
+    /**
+     * Deletes a user from the "users" table by their id.
+     *
+     * @param user The user to be deleted.
+     */
     @Override
     public void deleteUser(User user) {
         try {
-            PreparedStatement statement = connection.prepareStatement("DELETE FROM users WHERE id = ?");
+            PreparedStatement statement = connection.prepareStatement(
+                    "DELETE FROM users WHERE id = ?"
+            );
             statement.setInt(1, user.getId());
             statement.executeUpdate();
         } catch (Exception e) {
@@ -93,10 +131,18 @@ public class SqliteUserDAO implements IUserDAO {
         }
     }
 
+    /**
+     * Retrieves a user from the "users" table by their id.
+     *
+     * @param id The id of the user to retrieve.
+     * @return The retrieved user, or null if not found.
+     */
     @Override
     public User getUser(int id) {
         try {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM users WHERE id = ?");
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT * FROM users WHERE id = ?"
+            );
             statement.setInt(1, id);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
@@ -104,10 +150,6 @@ public class SqliteUserDAO implements IUserDAO {
                 String lastName = resultSet.getString("lastName");
                 String username = resultSet.getString("username");
                 String password = resultSet.getString("password");
-//                String phone = resultSet.getString("phone");
-//                String email = resultSet.getString("email");
-//                String school = resultSet.getString("school");
-//                String address = resultSet.getString("address");
                 User user = new User(firstName, lastName, username, password);
                 user.setId(id);
                 return user;
@@ -118,6 +160,11 @@ public class SqliteUserDAO implements IUserDAO {
         return null;
     }
 
+    /**
+     * Retrieves all users from the "users" table.
+     *
+     * @return A list of all users.
+     */
     @Override
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
@@ -129,9 +176,9 @@ public class SqliteUserDAO implements IUserDAO {
                 int id = resultSet.getInt("id");
                 String firstName = resultSet.getString("firstName");
                 String lastName = resultSet.getString("lastName");
-                String phone = resultSet.getString("phone");
-                String email = resultSet.getString("email");
-                User user = new User(firstName, lastName, phone, email);
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
+                User user = new User(firstName, lastName, username, password);
                 user.setId(id);
                 users.add(user);
             }
@@ -141,36 +188,46 @@ public class SqliteUserDAO implements IUserDAO {
         return users;
     }
 
+    /**
+     * Checks if a user with the given username exists in the database.
+     *
+     * @param userName The username to check.
+     * @return True if the user exists, false otherwise.
+     */
     @Override
     public boolean userExists(String userName) {
         try {
             String query = "SELECT * FROM users WHERE username = '" + userName + "'";
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
+            return resultSet.next();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
+    /**
+     * Validates the provided password for the given username.
+     *
+     * @param userName The username whose password needs to be validated.
+     * @param password The password to validate.
+     * @return True if the password matches, false otherwise.
+     */
+    @Override
+    public boolean passwordCorrect(String userName, String password) {
+        try {
+            String query = "SELECT * FROM users WHERE username = '" + userName + "'";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
             if (resultSet.next()) {
-                return true;
+                String storedPassword = resultSet.getString("password");
+                return storedPassword.equals(password);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return false;
     }
-
-    @Override
-    public boolean passwordCorrect(String userName, String password)
-    {
-        try {
-            String query = "SELECT * FROM users WHERE username = '" + userName + "'";
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
-
-            String storedPassword = resultSet.getString("password");
-            return storedPassword.equals(password);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
 }
+
